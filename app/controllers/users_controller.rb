2006a2ea3d2e51d4436df9553e1456562a89bf2a
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-04-12 08:02:48 +0000

    [Finish]4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])  
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+def edit
+    @user = User.find(params[:id]）
+end  
   
   def update
     @user = User.find(params[:id])
@@ -21,3 +24,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
 end
+  
+def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
